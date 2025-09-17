a555813c79901e1eb200a8787ebf6057c90cc63e
vivekbhadauriya <vkthakur6.7.9.8@gmail.com>
2025-09-17 23:42:09 +0530

    Button Hook Created

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,13 @@
 import RestroCard from "./RestroCard";
 import ResList from "../Utils/ResList";
 import { HERO_URL } from "../Utils/constant";
+import { useState } from "react";
 
 const Body = () => {
+    const[list,setlist] = useState(ResList)
+    console.log(list);
+    
+
     return (
         <>
         <section className="hero-section">
@@ -12,11 +17,17 @@ const Body = () => {
                 <button className="hero-btn">Order Now</button>
             </div>
             <div className="hero-image">
-                <img src= {HERO_URL}alt="Delicious Food" />
+                <img src= {HERO_URL} alt="Delicious Food" />
             </div>
         </section>
+        <div className="filter-btn">
+            <button className="filter" onClick={() => setlist(list.filter((res) => res.info.avgRating > 4.2))}>
+                   
+              Top Rated Restro </button>
+        </div>
+
         <div className="restro-block">
-            {ResList.map(({info}) => (
+            {list.map(({info}) => (
                 <RestroCard key={info.id} {...info} deliveryTime={info.sla?.deliveryTime} />
             ))}
         </div>
